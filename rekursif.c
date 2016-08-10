ff365216fda91252afa9fe62a7d103c9fc9e42b9
danielseptry <septrypanjaitan@gmail.com>
2016-08-09 18:39:31 -0700

    edit

--- rekursif.c
+++ rekursif.c
@@ -3,22 +3,22 @@
 
 int faktorial (int a);
 
+
 main()
 {
 	int n;
-
+	
 	printf ("masukkan bilangan; ");
 	scanf("%d", &n);
 	printf("faktorial %d = %d", n, faktorial(n));
 	getch();
 }
 
-int faktorial (int b)
+int faktorial (int a)
 {
 	if(a==0) {
 		return 1;
 	}else {
-	return b * faktorial (b - 1);
-		return a * faktorial (b - 1);
+		return a * faktorial (a - 1);
 	}
 }
